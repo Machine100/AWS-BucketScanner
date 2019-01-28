78ec5102c203dd30ff14f0a36014111a392e2c60
machine100 <joesmileyyy@yahoo.com>
2019-01-28 11:06:37 -0800

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -4,13 +4,13 @@ import boto3
 s3 = boto3.client('s3')
 
 response = s3.list_buckets()
-print('---rcvd from s3.list_buckets()--->', response)  #response comes in as a dict
+print('<---rcvd from s3.list_buckets()---', response)  #response comes in as a dict
 
 for key in response:    
-	print(key, '<---key from list_buckets')            #display the keys in that dict
+	print(key, '---key from list_buckets')            #display the keys in that dict
 
 response2 = response['Buckets']                        #from key 'Buckets', extract a list
-print('---rcvd the Buckets key--->', response2)
+print('<---rcvd the Buckets key---', response2)
 
 print('These are the buckets in your account:')        #display key 'Name' for each item in that list 
 for item in response2:                                 #these are the bucketnames for the account configured
