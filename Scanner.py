168e6f323d7c45f8d4b710cde1b396eb60db52eb
machine100 <joesmileyyy@yahoo.com>
2019-01-29 18:19:22 -0800

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,24 +1,10 @@
 
 
 import boto3
-s3 = boto3.client('s3')                                #nstantiate a client connection to S3
+s3 = boto3.client('s3')                                #instantiate a client connection to S3
 
-response = s3.list_buckets()
-print('<---rcvd from s3.list_buckets()---', response)  #response comes in as a dict
+#open a wordlist file
+#copy words into a list
+#make API call (TBD) for each word
+#record hits into a list
 
-response2 = response['Buckets']                        #from key 'Buckets', extract a list
-print('<---rcvd the Buckets key---', response2)
-
-print('These are the buckets in your account:')        #display key 'Name' for each item in that list 
-for item in response2:                                 #these are the bucketnames for the account configured
-	print(item['Name'])                                #under the pc's AWS console configuration.
-
-  
-
-	  
-
-#response = s3.list_buckets()
-
-#buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-#print('Bucket List: %s' % buckets)                  
