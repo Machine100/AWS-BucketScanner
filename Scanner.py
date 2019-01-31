02793c3b1cebdfc2a144dbb95e1cb6e116064763
Machine100 <joesmileyyy@yahoo.com>
2019-01-31 09:19:54 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -3,8 +3,18 @@
 import boto3
 s3 = boto3.client('s3')                                #instantiate a client connection to S3
 
-#open a wordlist file
-#copy words into a list
+#open a wordlist file using python's built in open() function
+file = open('wordlist.txt')
+print(file)                       #display the object's metadata?
+print(file.name)                  #call a method on the open() object
+wordlist=file.read().split('\n')  #call a method on the open() object to genearte the desired list. Newline characters are stripped.   
+print(wordlist)                   
+
+#need to clear the /n that was placed in the strings
+#for i in wordlist:
+
+
+
 #make API call (TBD) for each word
 #record hits into a list
 
