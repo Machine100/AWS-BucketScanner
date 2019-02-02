a59e4f89a564301926402aceb5d61ba5b57206a0
Machine100 <39662431+Machine100@users.noreply.github.com>
2019-02-02 13:15:14 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,16 +1,26 @@
 
 
 import boto3
+import botocore
 s3 = boto3.client('s3')                                #instantiate a client connection to S3
 
 #open a wordlist file using python's built in open() function
 file = open('wordlist.txt')
 print(file)                       #display the object's metadata?
 print(file.name)                  #call a method on the open() object
-wordlist=file.read().split('\n')  #call a method on the open() object to genearte the desired list. Newline characters are stripped.   
-print(wordlist)                   
+words=file.read().split('\n')  #call a method on the open() object to genearte the desired list. Newline characters are stripped.   
+#print(words)                   
 
-#need to clear the /n that was placed in the strings
+for i in words:     bucket = s3.Bucket('word')     exists = true     try:
+s3.meta.client.head_bucket('name')     
+
+except botocore.exceptions.ClientError as e:         
+	# if a client error is thrown, then check that it was a 404 error
+	# if it was a 404 error, then the bucket does not exist
+	error_code = e.response['Error']['Code']         
+	if error_code == '404':
+		exists=false
+
 #for i in wordlist:
 
 
