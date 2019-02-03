21600a939e932c6bd1f965ca58824844ad578209
machine100 <joesmileyyy@yahoo.com>
2019-02-03 12:59:38 -0800

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -2,27 +2,24 @@
 
 import boto3
 import botocore
-s3 = boto3.client('s3')           #instantiate a client connection to S3
-file = open('wordlist.txt')       #open a wordlist file using python's built in open() function
-print(file)                       #display the object's metadata?
-print(file.name)                  #call a method on the open() object
-words=file.read().split('\n')     #call a method on the open() object to genearte the desired list. Newline characters are stripped.   
+s3client = boto3.client('s3')           #instantiate a client connection to S3
+s3resource = boto3.resource('s3')       #instantiate a resource connection to S3
+file = open('wordlist.txt')             #open a wordlist file using python's built in open() function
+print(file)                             #display the object's metadata?
+print(file.name)                        #call a method on the open() object
+words=file.read().split('\n')           #call a method on the open() object to genearte the desired list. Newline characters are stripped.   
 #print(words)                   
 
 for word in words:
-	#bucket = s3.Bucket(word)     
-	exists = true     
+	exists = True 
 	try:
-		s3.meta.client.head_bucket('name')     
+		s3resource.meta.client.head_bucket(Bucket=word)     #This is the official way to check if a bucket exists
+	except botocore.exceptions.ClientError as e:            #Which is to say go through the resource object.
+		error_code = e.response['Error']['Code']                
+		if error_code == '404': 
+			exists=False
+	print ('Bucket', word,':', exists) 
 
-	except botocore.exceptions.ClientError as e:         
-		# if a client error is thrown, then check that it was a 404 error
-		# if it was a 404 error, then the bucket does not exist
-		error_code = e.response['Error']['Code']         
-		if error_code == '404':
-			exists=false
-
-#for i in wordlist:
 
 
 
